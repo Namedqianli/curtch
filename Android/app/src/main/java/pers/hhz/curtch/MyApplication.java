137fdd2cb25d205b572a0d0dd19995196fbc3d7b
Mr.Li <lijy9811@qq.com>
2020-01-11 13:30:22 +0800

    init

---
+++ Android/app/src/main/java/pers/hhz/curtch/MyApplication.java
@@ -0,0 +1,14 @@
+package pers.hhz.curtch;
+
+import android.app.Application;
+
+import com.qmuiteam.qmui.arch.QMUISwipeBackActivityManager;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        QMUISwipeBackActivityManager.init(this);
+    }
+}
